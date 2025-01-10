bccc0ae9b9d95b3f2df721d83719f6b06b85e2ef
gabrielregis3 <txiloo3@gmail.com>
2025-01-10 18:09:32 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
+      <main className="my-10 w-full md:max-w-2x1">
+        <h1 className="text-4x1 font-medium text-white">Clients</h1>
+      </main>
+    </div>
+  )
+}
