22d83faa71f10af12bcfc68f78d5a18c69614f40
gabrielregis3 <txiloo3@gmail.com>
2025-01-14 16:06:37 -0300

    final things and english texts

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,90 @@
+import { useEffect, useState, useRef, FormEvent } from "react"
 import { FiTrash } from "react-icons/fi"
+import { api } from './services/api'
+
+interface CustomerProps {
+  id: string;
+  name: string;
+  email: string;
+  status: boolean;
+}
 
 export default function App() {
+
+  const [customers, setCustomers] = useState<CustomerProps[]>([]);
+  const nameRef = useRef<HTMLInputElement | null>(null)
+  const emailRef = useRef<HTMLInputElement | null>(null)
+
+  useEffect(() => {
+    loadCustomers();
+  }, [])
+
+  async function loadCustomers() {
+    const response = await api.get("/customers");
+    setCustomers(response.data);
+  }
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    if (!nameRef.current?.value || !emailRef.current?.value) return
+    
+    const response = await api.post("/customer", {
+      name: nameRef.current?.value,
+      email: emailRef.current?.value
+    })
+
+    setCustomers(allCustomers => [...allCustomers, response.data])
+
+    nameRef.current.value = ""
+    emailRef.current.value = ""
+    
+  }
+
+  async function handleDelete(id: string) {
+
+    try {
+      await api.delete("/customer", {
+        params: {
+          id: id
+        }
+      })
+
+      const allCustomers = customers.filter( (customer) => customer.id !== id)
+      setCustomers(allCustomers)
+
+    } catch (err) {
+        throw new Error("An error occurred while deleting the customer.");
+    }
+  }
+
   return (
     <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
       <main className="my-10 w-full md:max-w-2x1">
-        <h1 className="text-4xl font-medium text-white">Clientes</h1>
+        <h1 className="text-4xl font-medium text-white">Clients</h1>
 
-        <form className="flex flex-col my-6">
-          <label className="font-medium text-white">Nome:</label>
-          <input type="text" placeholder="Digite seu nome..." className="w-full mb-5 p-2 rounded"/>
+        <form className="flex flex-col my-6" onSubmit={handleSubmit}>
+          <label className="font-medium text-white">Name:</label>
+          <input type="text" placeholder="Enter your name..." className="w-full mb-5 p-2 rounded" ref={nameRef}/>
 
           <label className="font-medium text-white">Email:</label>
-          <input type="email" placeholder="Digite seu Email..." className="w-full mb-5 p-2 rounded"/>
+          <input type="email" placeholder="Enter your email..." className="w-full mb-5 p-2 rounded" ref={emailRef}/>
 
-          <input type="submit" value="Cadastrar" className="cursor-pointer w-full p-2 bg-green-500 rounded font-medium" />
+          <input type="submit" value="Register" className="cursor-pointer w-full p-2 bg-green-500 rounded font-medium" />
         </form>
 
-        <section className="flex flex-col">
-          <article className="w-full bg-white rounded p-2 relative hover:scale-105 duration-200">
-            <p><span>Nome:</span></p>
-            <p><span>Email:</span></p>
-            <p><span>Status:</span></p>
-
-            <button className="bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute right-0 -top-2">
-              <FiTrash size={18} color="#FFF"/>
-            </button>
-          </article>
+        <section className="flex flex-col gap-4">
+          {customers.map( (customer) => (
+              <article key={customer.id} className="w-full bg-white rounded p-2 relative hover:scale-105 duration-200">
+              <p><span className="font-medium">Name:</span> {customer.name}</p>
+              <p><span className="font-medium">Email:</span> {customer.email}</p>
+              <p><span className="font-medium">Status:</span> {customer.status ? "ACTIVE" : "INACTIVE"}</p>
+  
+              <button className="bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute right-0 -top-2" onClick={() => handleDelete(customer.id)}>
+                <FiTrash size={18} color="#FFF"/>
+              </button>
+            </article>
+          ))}
         </section>
       </main>
     </div>
